ebbd223ad6af3d817f840f9d5805d2f843de4e08
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-07 15:37:19 -0300

    puxando produto mais barato do banco

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,5 +2,6 @@ class ProdutosController < ApplicationController
 
     def index
         @produtos = Produto.order :nome
+        @produto_com_desconto = Produto.order(:preco).limit 1
     end
 end
