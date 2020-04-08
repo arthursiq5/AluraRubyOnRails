b4aa123601653f96b0ba8149c40a8c0c12518be0
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-07 23:52:10 -0300

    aperfeiçoando formulário para envio correto

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "produtos", to: "produtos#index"
   get "produtos/new", to: "produtos#new"
+  post "produtos", to: "produtos#create"
   root to: "produtos#index"
 end
