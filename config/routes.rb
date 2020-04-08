f86ade13ece736f3e1d14f7f5129489abde01671
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-08 15:52:01 -0300

    melhorada função de deletar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "produtos", to: "produtos#index"
   get "produtos/new", to: "produtos#new"
-  get "produtos/:id/remover", to: "produtos#destroy"
+  delete "produtos/:id", to: "produtos#destroy", as: :produto
   post "produtos", to: "produtos#create"
   root to: "produtos#index"
 end
