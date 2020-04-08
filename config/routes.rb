9614c7c4f9648eb3259b021c5e18ab0b74e2652f
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-08 16:28:48 -0300

    reconfigurando rotas usando helpers do rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "produtos", to: "produtos#index"
-  get "produtos/new", to: "produtos#new"
-  delete "produtos/:id", to: "produtos#destroy", as: :produto
-  post "produtos", to: "produtos#create"
+  resources :produtos, only: [:new, :create, :destroy]
   root to: "produtos#index"
 end
