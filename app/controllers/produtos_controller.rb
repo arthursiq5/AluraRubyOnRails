f097ff7d6c1b2b438644f3a666530fc48f47eabf
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-07 23:53:38 -0300

    redirecionando para raiz

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -8,5 +8,6 @@ class ProdutosController < ApplicationController
     def create
         produto = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
         Produto.create produto
+        redirect_to root_path
     end
 end
