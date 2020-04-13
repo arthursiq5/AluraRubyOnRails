3838fcfc54ec9168b4a95b0b640bf5fd9a20fb3e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-13 15:15:40 -0300

    criando departamentos através de scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_170126) do
+ActiveRecord::Schema.define(version: 2020_04_13_172755) do
+
+  create_table "departamentos", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
