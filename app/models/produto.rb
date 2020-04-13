5605cc8aa7db94344528a2063f157286404ac667
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-13 17:09:46 -0300

    relacionando departamento à produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,7 @@
 class Produto < ApplicationRecord
+
+    belongs_to :departamento
+
     validates :preco, presence: true
     validates :nome, presence: true
     validates :descricao, length: { in: 5..200 }
