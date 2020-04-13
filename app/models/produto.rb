11afb475d4f36d30f53f88c2a51da4ac3d153ab1
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-13 13:52:28 -0300

    configuradas as demais validações

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,3 +1,6 @@
 class Produto < ApplicationRecord
     validates :preco, presence: true
+    validates :nome, presence: true
+    validates :descricao, length: { in: 5..200 }
+    validates :quantidade, numericality: { only_integer: true }
 end
