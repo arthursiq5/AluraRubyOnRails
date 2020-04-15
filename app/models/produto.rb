ae683933ed6148ee5a1db40fc0e5056cb5cfd4dc
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-15 17:18:40 -0300

    adicionado item em validações

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,7 +2,7 @@ class Produto < ApplicationRecord
 
     belongs_to :departamento, optional: true
 
-    validates :preco, :nome, presence: true
+    validates :preco, :nome, :departamento, presence: true
     validates :descricao, length: { in: 5..200 }
     validates :quantidade, numericality: { only_integer: true }
 end
