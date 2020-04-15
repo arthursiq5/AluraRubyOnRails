4bab738c94a02b77a9e76b339bd8597c5116b428
Arthur Siqueira <arthur.siq5@gmail.com>
2020-04-15 17:11:15 -0300

    corrigir formatação dos validadores

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,8 +2,7 @@ class Produto < ApplicationRecord
 
     belongs_to :departamento, optional: true
 
-    validates :preco, presence: true
-    validates :nome, presence: true
+    validates :preco, :nome, presence: true
     validates :descricao, length: { in: 5..200 }
     validates :quantidade, numericality: { only_integer: true }
 end
